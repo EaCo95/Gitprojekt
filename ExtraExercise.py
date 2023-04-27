088cf2d609b99edcafa27f8f4f1f2f7d23883aa5
EaCo95 <ea_compton@yahoo.com>
2023-04-27 13:47:59 +0200

    aa

---
+++ ExtraExercise.py
@@ -0,0 +1,103 @@
+MenyA = {1: Skapa konto', 2: 'Logga in', 3:'Avsluta'}
+
+print(MenyA)
+
+
+
+
+# print("*** Välkommen! ***")
+
+# def MenyA():
+    
+#     Kontonummer = "2. Logga in"
+       
+#     while True:    
+
+#         print("1. Skapa konto")
+#         print("2. Logga in")
+#         print("3. Avsluta")
+
+#         action = input("Vad vill du göra?")
+
+#         if action == "1":
+              
+#                 print("Input ange kontonummer")
+#                 print("Om redan taget" "Felmeddelande")
+#                 print("Annars skapa och tillbaka till meny A")
+
+#         if action == "2":
+                
+#                 input("Ange kontonummer: ")
+
+#         if Kontonummer == True:
+
+#               print("OK och gå till MenyB")
+
+#         if action == "3":
+              
+#               break
+
+                
+
+                
+# - Input ange kontonummer
+# - Om ej finns "Felmeddelande"
+# - Annars OK och gå till meny B
+
+
+
+
+
+
+
+
+       # def MenyB():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    print("1. Skapa konto")
+    print("2. Logga in")
+    print("3. Avsluta")
+
+action = input("vad vill du göra:")
+if action == "1":
+        print("*** Skapa konto ***")
+        input("Ange kontonummer: ")
+elif
+       print("Felmeddelande")
+
+else:
+       print("OK och gå till meny B.")
+    
+if action == "2":
+        nummer = 1 
+# print("Meny A") 
+#       "1 Skapa konto"
+#       "2 Logga in"
+#       "3 Avsluta"
+
+# print("Meny B")
+
+
+
+
+# print("Meny C")
+
+
+
+# print("Meny D")
